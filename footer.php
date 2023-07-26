effdcb3059f291ebd231f46d1f437930f2128bd4
Shrey Patel <117505738+shrey-396@users.noreply.github.com>
2023-07-26 19:21:07 +0530

    updated

---
+++ footer.php
@@ -0,0 +1,5 @@
+<p style="margin-top:50px;">Project By: 
+    <a>Shrey Patel</span></a>, 
+  <a><span class="label label-info">Prince Kalda</span></a>, 
+  <a><span class="label label-info">Yash Patel</span></a> 
+</p>
